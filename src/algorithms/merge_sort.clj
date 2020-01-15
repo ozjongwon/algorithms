6aef933ff9d7b61bf78c2b97abdc5863047ad2ce
Jongwon Choi <oz.jongwon.choi@gmail.com>
2020-01-15 15:21:44 +1100

    Add merge sort

---
+++ src/algorithms/merge_sort.clj
@@ -0,0 +1,34 @@
+(ns algorithms.merge-sort
+  (:refer-clojure :exclude [sort merge]))
+
+(defn merge [^objects arr1 ^objects arr2 l m h]
+  (let [lupper (inc m)
+        rupper (inc h)]
+    (loop [i l j (inc m) k l]
+      (cond (> i m) (do
+                      (doseq [[j k] (mapv list (range j rupper) (range k rupper))]
+                        (aset arr2 k (aget arr1 j)))
+                      arr2)
+
+            (> j h) (do
+                      (doseq [[i k] (mapv list (range i lupper) (range k rupper))]
+                        (aset arr2 k (aget arr1 i)))
+                      arr2)
+
+            (< (aget arr1 j) (aget arr1 i)) (do (aset arr2 k (aget arr1 j))
+                                                (recur i (inc j) (inc k)))
+
+            :else
+            (do (aset arr2 k (aget arr1 i)) (recur (inc i) j (inc k)))))))
+
+(defn sort [^objects arr]
+  (let [n (alength arr)
+        aux (object-array n)]
+    (loop [sz 1 arr1 arr arr2 aux]
+      (if (< sz n)
+        (do (loop [l 0]
+              (when (<= l (- n sz))
+                (merge arr1 arr2 l (dec (+ l sz)) (min (dec (+ l sz sz)) (dec n)))
+                (recur (+ l sz sz))))
+         (recur (+ sz sz) arr2 arr1))
+        arr1))))
